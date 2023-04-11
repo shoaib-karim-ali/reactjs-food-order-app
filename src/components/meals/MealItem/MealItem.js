56c313287c78132fa8c6b2d3283118a130051972
shoaib <shoaibkarimali@gmail.com>
2023-04-11 10:50:24 +0500

    Use MealItem component

--- src/components/meals/MealItem/MealItem.js
+++ src/components/meals/MealItem/MealItem.js
@@ -1,3 +1,4 @@
+import MealItemForm from './MealItemForm';
 import style from './MealItem.module.css';
 
 const MealItem = props => {
@@ -6,11 +7,13 @@ const MealItem = props => {
     return (
         <li className={style.meal}>
             <div>
-            <h3>{props.name}</h3>
-            <div className={style.description}>{props.description}</div>
-            <div className={style.price}>{price}</div>
+                <h3>{props.name}</h3>
+                <div className={style.description}>{props.description}</div>
+                <div className={style.price}>{price}</div>
             </div>
-            <div></div>
+            <div>
+                <MealItemForm id={props.id} />
+            </div>
         </li>
     );
 }
